14dedcd698ce4e6f9197c73fd83621d26044b9ea
Emi09 <emiliofrancescolisciotto@gmail.com>
2022-12-10 13:44:28 +0100

    things

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'className="App-logo" alt="logo" />
         <p>
            Hi, <b>I'm &mi!</b><br></br>I'm a guy from Italy<br></br>that loves to code.
         </p>
