241f0bd75cd510807aa29efa98e4437cff892f45
Emi09 <emiliofrancescolisciotto@gmail.com>
2022-12-10 14:26:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Buttonpro from './components/buttonpro';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
 
       <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src={logo} className="App-logo" alt="logo" /></a>
 
@@ -26,7 +26,7 @@ function App() {
          <div class="neve"></div>
         </div>
 
-      </header>
+      </div>
     </div>
   );
 }
